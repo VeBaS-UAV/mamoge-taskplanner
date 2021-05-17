327afca750a54be3288702889056aa50429d5109
Matthias Füller <matthias.fueller@fhdw.de>
2021-05-17 22:58:30 +0200

    refactored

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 import setuptools
 
+print(setuptools.find_packages(where="."))
+print(setuptools.find_namespace_packages(include=["mamoge/*","tests/*"]))
+
+packages = [package for package in setuptools.find_namespace_packages(where='mamoge', include='mamoge.*')]
+print(packages)
 
 setuptools.setup(
     name="mamoge-taskplanner",
@@ -17,7 +22,8 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
+    package_dir={"mamoge.taskplanner": "./mamoge/taskplanner", "mamoge.taskplanner.tests":"./tests/helpers"},
+    #packages=["mamoge.taskplanner", "mamoge.taskplanner.nx", "mamoge.taskplanner.optimize" ,"mamoge.taskplanner.tests"],
+    packages=setuptools.find_packages(where="."),
     python_requires=">=3.9",
 )
---
+++ tests/test_graph_build.py
@@ -0,0 +1,6 @@
+import helpers.graph_helper
+
+
+def test_answer():
+
+    assert True
