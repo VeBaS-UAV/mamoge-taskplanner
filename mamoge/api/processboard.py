537cac2c691c715bfdd4f78449660034c6fe6d46
Pascal Niggemeier <pascal.niggemeier@fhdw.de>
2023-01-19 17:32:39 +0000

    #11 introduce the processboard API

--- mamoge/api/processboard.py
+++ mamoge/api/processboard.py
@@ -1,8 +1,64 @@
-from mamoge.models.tasks import DAG, Task
+"""MamoGe Process Board API.
+
+This API handles the process of managing processing (here processes in the sense of
+ordered Tasks that are to be done by workers.)
+"""
+import redis
+
+from mamoge.models.tasks import DAG
 
 
 class ProcessBoardAPI:
-    def __init__(self) -> None:
-        pass
+    """Base class for the ProcessBoardAPI."""
 
-    def 
+    def run_process(self, name: str, template: DAG):
+        """Instantiate and run the given named process.
+
+        This will create a process instance, which will be run.
+
+        Args:
+            name (str): A unique name to identify the process.
+            template: DAG
+        """
+        raise NotImplementedError
+
+    def get_process_list(self):
+        """Get the list of currently running processes.
+
+        This information can be used for listing the processes in the GUI service.
+
+        If looking for not only the running but the planned as well, please ask the
+        process director service.
+        """
+        raise NotImplementedError
+
+    def update(self, name: str, template: DAG):
+        """Update the given process.
+
+        Unlike `run_process` this function allows to update a running process.
+        Running processes
+        """
+        raise NotImplementedError
+
+    def delete_process(self, name: str):
+        """Delete the given process.
+
+        A delete is equivalent to aborting a process.
+
+        Args:
+            name (str): A unique name to identify the process.
+        """
+        raise NotImplementedError
+
+
+class ProcessBoardRedisAPI(ProcessBoardAPI):
+    """The overlay for the WorkerAPI with a redis flavor.
+
+    This WorkerAPI holds a connection to a redis database.
+    """
+
+    def __init__(self, host="localhost", port=6379):
+        """Initialize."""
+        super().__init__()
+        self.redis = redis.Redis(host, port)
+        self.tasks = list()
--- tests/test_processboard.py
+++ tests/test_processboard.py
@@ -1,67 +1,30 @@
-import pytest
-
-from mamoge.api.worker import WorkerAPI, RedisWorker
+from mamoge.api.processboard import ProcessBoardAPI, ProcessBoardRedisAPI
 from mamoge.models.capabilities import Capabilities, Capability
-from mamoge.models.tasks import Task
 
 
-class BaseProcessBoard:
+class BaseProcessBoardAPI:
     """Global test cases that are meant to be run for every child class.
 
-    child classes are different imlpementations of the workerAPI, e.g. with different
-    connections and corresponding specifications.
+    Child classes are different implementations of the Baseclass,
+    e.g. with different DB connections and corresponding specifications.
     """
 
     @classmethod
     def setup_class(cls):
-        cls.worker = WorkerAPI()
+        cls.board = ProcessBoardAPI()
 
         cls.cap_water = Capability("water", 10)
         cls.cap_energy = Capability("energy", 15)
         cls.cap_multiple = Capabilities(cls.cap_water, cls.cap_energy)
 
-        cls.t1 = Task(id=, name=, requirements=, state=)
 
-    def test_register(self):
-        result = self.worker.register("username", self.cap_multiple)
-        assert result is True
+class Test_ProcessBoardRedis(BaseProcessBoardAPI):
+    """Implements the Process Board with a Redis connection.
 
-    def test_unregister(self):
-        result = self.worker.unregister("username")
-        assert result is True
+    It inherits the base classes' test functions. Therefore only test for function
+    implementations that are specific for the redis connection are defined here.
+    """
 
-    def test_keep_alive(self) -> None:
-        result = self.worker.keep_alive("username")
-        assert result is True
-
-    def test_task_update(self):
-        id
-        status
-
-        result = self.worker.task_update(id, status)
-        assert result is True
-
-    def test_tasks_receive(self):
-        assert True is True
-
-    def test_update_capabilities(self):
-
-        result = self.worker.update_capabilities("username", self.cap_multiple)
-        assert result is True
-
-    def test_update_capability(self):
-        result = self.worker.update_capability("username", self.cap_water)
-        assert result is True
-
-
-class Test_Redis(BaseWorkerAPI):
     @classmethod
     def setup_class(cls):
-        cls.worker = RedisWorker("name")
-
-
-class Test_Postgressql(BaseWorkerAPI):
-    @classmethod
-    def setup_class(cls):
-        cls.worker = WorkerAPI()
-
+        cls.worker = ProcessBoardRedisAPI("name")
