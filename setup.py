4da67f60378c412c505417792e22d36b82689df2
Pascal Niggemeier <pascal.niggemeier@fhdw.de>
2023-01-19 07:55:28 +0000

    #3 update project description

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ import setuptools
 setuptools.setup(
     name="mamoge-taskplanner",
     version="0.0.1",
-    author="matthias fueller",
-    author_email="matthias.fueller@fhdw.de",
+    author="matthias fueller, pascal niggemeier",
+    author_email="matthias.fueller@fhdw.de, pascal.niggemeier@fhdw.de",
     description="mamoge task planner",
-    long_description="TBD",
+    long_description="""MamoGe Taskplanner is a package that is a colletion of service
+    templates and designs for delivering dynamic and robust task allocation and
+    management.""",
     long_description_content_type="text/markdown",
     url="https://github.com/TBD",
-    project_urls={
-        "Bug Tracker": "https://github.com/TBD",
-    },
+    project_urls={"Bug Tracker": "https://github.com/MamoGe/mamoge-taskplanner/issues"},
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
